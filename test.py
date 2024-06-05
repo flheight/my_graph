5ccda70bdf2364d1356167dc6bba3b126e53d9c5
flheight <75139846+flheight@users.noreply.github.com>
2024-06-05 11:48:07 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+from sklearn.metrics import adjusted_rand_score
+from graph import Graph
+
+np.random.seed(0)
+
+X, y = make_circles(n_samples = 2048, factor=.5, noise=.05)
+
+plt.scatter(X[:, 0], X[:, 1], color='black', s=.5)
+
+net = Graph(n_classes=2)
+
+net.fit(X, 16, lam=3e-2)
+
+colors = plt.cm.tab10(np.arange(2))
+
+guess = net.predict(X)
+
+for i in range(2):
+    plt.scatter(net.clusters[i][:, 0], net.clusters[i][:, 1], color=colors[i], label=f'Cluster {i}')
+
+plt.show()
+
+
+ari = adjusted_rand_score(y, guess)
+print(f"Adjusted Rand Index: {ari}")
